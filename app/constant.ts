e5a94ce990c998bde14f08702b45fece1aea930d
saifudingultom <130130481+saifudingultom@users.noreply.github.com>
2023-04-12 07:32:08 -0400

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "robert.media378";
 export const REPO = "";
 export const REPO_URL = `https://instagram.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://instagram.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const UPDATE_URL = `${REPO_URL}`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
